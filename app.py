cbc7823d1aaaa0ea10a3d04704c79dbdf12bd5c3
Yasamin Akrami <111709775+yasakrami@users.noreply.github.com>
2024-08-08 11:50:40 +0300

    change public link

--- app.py
+++ app.py
@@ -39,5 +39,6 @@ def process_mesh():
 
     return jsonify({'error': 'Invalid file format. Only .obj files are allowed.'}), 400
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
